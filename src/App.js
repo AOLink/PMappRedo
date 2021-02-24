9403fec560ccda72e11bbee834e82609cd762173
AOLink <aolink101@gmail.com>
2021-02-23 19:02:54 -0500

    stactic data for PL component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard'
 
 
 class App extends Component {
@@ -8,7 +9,10 @@ class App extends Component {
       return (
         <BrowserRouter>
           <div className="App">
-            <Navbar />
+            <Navbar /> 
+            <Switch>
+                <Route path='/' component={Dashboard} />
+            </Switch>
           </div>
         </BrowserRouter>
       )
---
+++ src/components/dashboard/Notifications.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Notifications = () => {
+    return(
+        <div className="">
+            <p>Notification</p>
+        </div>
+    )
+}
+
+export default Notifications 
